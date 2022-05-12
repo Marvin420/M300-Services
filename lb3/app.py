3e89eeda60cf743cc7fa12434192c408e962a47a
Marvin420 <100407598+Marvin420@users.noreply.github.com>
2022-05-12 08:31:54 +0200

    Update app.py

--- lb3/app.py
+++ lb3/app.py
@@ -20,4 +20,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return 'Grüezi Herr Berger! Diese Seite wurde {} mal aktualisiert.\n'.format(count)
